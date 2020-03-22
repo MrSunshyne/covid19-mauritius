da05affba78c50aa7e8abba5a552499f9123364e
Sandeep Ramgolam <sandeep@lsl.digital>
2020-03-22 15:57:44 +0400

    charts and footer

--- src/components/chart-new-cases.vue
+++ src/components/chart-new-cases.vue
@@ -2,7 +2,49 @@
   <div class="py-8 bg-gray-100">
     <div class="container mx-auto ">
       <h2 class="text-2xl leading-none pb-8">New Cases</h2>
-      <div class="chart w-full h-64 bg-red-300"></div>
+      <div class="chart-container w-full h-64 bg-red-300 relative">
+        <line-chart :chart-data="datacollection"></line-chart>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import LineChart from "../helpers/LineChart";
+
+export default {
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      datacollection: null
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()]
+          },
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      };
+    },
+    getRandomInt() {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    }
+  }
+};
+</script>
--- src/helpers/LineChart.js
+++ src/helpers/LineChart.js
@@ -4,7 +4,7 @@ const { reactiveProp } = mixins
 export default {
     extends: Line,
     mixins: [reactiveProp],
-    props: ['options'],
+    props: ['options', 'chartData'],
     mounted () {
         // this.chartData is created in the mixin.
         // If you want to pass options please create a local options object
