fd1c526f9744f318b02b44a7000f51f8a0b2f393
Sandeep Ramgolam <sandeep@ramgolam.com>
2020-04-17 15:29:43 +0400

    updated sitemap for better seo

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+// Generate new route objects based on country names
+const cases = require('./src/data/countries.json' )
+const countryRoutes = cases.countries.map((routeSlug) => {
+    return {
+        path: '/country/' + routeSlug,
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: '0.8',
+        changefreq: 'hourly'
+    }
+});
+
 const paths = [
     {
         path: '/',
@@ -6,7 +18,10 @@ const paths = [
         priority: '0.8',
         changefreq: 'hourly'
     },
+ ...countryRoutes
 ];
+
+console.log(paths)
 
 module.exports = {
     configureWebpack: {
